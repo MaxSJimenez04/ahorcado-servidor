acff0ca1eec27608ec178366cc54b45c71b464e8
MaxSJimenez04 <maximiliano.soto.j@gmail.com>
2026-06-05 15:53:29 -0600

    feat: agregar DTOs y servicio de palabra y categoria

---
+++ DTO/CategoriaDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace ServidorAhorcado.DTO
+{
+    [DataContract]
+    public class CategoriaDTO
+    {
+        [DataMember]
+        public int idCategoria { get; set; }
+
+        [DataMember]
+        public string categoriaES { get; set; }
+
+        [DataMember]
+        public string categoriaEN { get; set; }
+    }
+}
---
+++ DTO/PalabraDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace ServidorAhorcado.DTO
+{
+    [DataContract]
+    public class PalabraDTO
+    {
+        [DataMember]
+        public  int idPalabra {  get; set; }
+
+        [DataMember]
+        public string palabraES { get; set; }
+            
+        [DataMember]
+        public string palabraEN { get; set; }
+
+        [DataMember]
+        public string descripcionES { get; set; }
+
+        [DataMember]
+        public string descripcionEN {  get; set; }
+
+        [DataMember]
+        public int categoriaId { get; set; } 
+    }
+}
