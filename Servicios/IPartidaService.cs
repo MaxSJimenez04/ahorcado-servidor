abd06e24ff32f5b457f78016200adc214c69f313
MaxSJimenez04 <maximiliano.soto.j@gmail.com>
2026-06-11 13:32:16 -0600

    Corregir estructura de proyecto y eliminar archivo de solución

--- Servicios/IPartidaService.cs
+++ Servicios/IPartidaService.cs
@@ -6,7 +6,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
-namespace ServidorAhorcado
+namespace ServidorAhorcado.Servicios
 {
     // Lo que el servidor le empuja al cliente
     [ServiceContract]
